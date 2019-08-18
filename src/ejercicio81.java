095221a7d97f1bf2c866dfd5d734865c060b4625
Vilma Camacho <vilmacamacho07@gmail.com>
2019-08-18 11:18:44 -0500

    Update ejercicio81.java
    Corrección de Código

--- src/ejercicio81.java
+++ src/ejercicio81.java
@@ -12,19 +12,19 @@ public class ejercicio81 {
     public static void main(String [ ] args) {
         Scanner entrada81 = new Scanner (System.in);
         
-        System.out.print("Ingresa un valor: ");
-        int x=entrada81.nextInt();
-        entrada81.close();
-               hfghfhfghf
-        System.out.println();
-        for (int i=1; i<=x; i++){
-            for(int j=1; j<=x-i; j++){
-                System.out.print(" ");
-            }
-            for (int t=1; t<=(i*2)-1; t++){
-                System.out.println("*");
-            }
-            System.out.println();
+        System.out.print("Ingresa la catidad de filas: ");
+        int filas=entrada81.nextInt();
+      
+       
+        int x= 0 ;
+             for (int i=0; i<filas; i++){
+                int j=0;
+                 while(j<=x){
+                  System.out.print("*");
+                  j++;
+                 }
+                x++;
+                System.out.println("");
         }
     }
 }
